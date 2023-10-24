083d18f78dbf99f3373f5ca367f8330415129b14
jacantsen <jacob.ac@sbcglobal.net>
2023-10-24 14:48:34 -0600

    removed secret key and made into env_var

--- Pig/settings.py
+++ Pig/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rvefegjpy#!c(sbdxozr@1str&5j$h=e3@dd%01(zm^*5fgqc+'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
