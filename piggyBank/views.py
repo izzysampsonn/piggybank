ea6468aed802b07875520fc281e252e46a07a136
jacantsen <jacob.ac@sbcglobal.net>
2023-10-24 18:26:48 -0600

    added home page, piggy bank page

--- piggyBank/views.py
+++ piggyBank/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')  # Render the home.html template
+
+
+def piggy_bank(request):
+    return render(request, 'piggybank.html')
